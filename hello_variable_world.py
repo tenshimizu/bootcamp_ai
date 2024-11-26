c57b15a4f67b27e834237b5f2d1527f44ccff7db
tenshimizu <tenshimizu@gmail.com>
2024-11-25 21:01:03 -0700

    Added hello_variable_world.py file for student activity

---
+++ hello_variable_world.py
@@ -0,0 +1,21 @@
+def hello_variable():
+    # Create a variable called 'full_name' that holds a string
+    full_name = "Erin Spencer-Priebe"
+    # Create a variable called 'country_to_visit' that holds a string
+    country_to_visit = "Egypt"
+    # Create a variable called 'days' that holds an integer of the number of days
+    # for the vacation
+    days = 14
+    # Create a variable called 'exchange_rate' that holds a float of the currency
+    # exchange rate for the country to visit    
+    exchange_rate = 0.020
+    # Create a variable called 'satisfied' that holds a Boolean
+    satisfied = True
+    # Print out "Hello <full_name>"
+    # Print out a sentence with the country the user entered
+    # Print out a sentence with the number of vacation days
+    # Print out a sentence with the currency exchange rate
+    # Print out a sentence with the value of satisfied
+    print(f"Hello {full_name}!\nI heard that you want to take a trip to {country_to_visit} for {days} days. That should be a lot of fun! \nJust so you know, the exchange rate in {country_to_visit} is {exchange_rate}/1 USD.\nAre you satisfied with these answers? {satisfied}")
+
+hello_variable()
